f06ab3aa5b84975b60b27a3acd41011187706105
nathants <me@nathants.com>
2019-01-22 19:44:29 -0800

    install requires gotta stay

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setuptools.setup(
     author_email='me@nathants.com',
     url='http://github.com/nathants/py-web',
     packages=['web'],
+    install_requires=['tornado==5.1.1'],
     description='a minimal, data centric web library'
 )
